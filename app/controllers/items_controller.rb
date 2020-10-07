bbdce9e387968c37293a7457bbb2bdb41be55dfc
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-07 10:17:52 +0100

    update
    re

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,13 @@
 class ItemsController < ApplicationController
-	before_action :find_item, only:[:show, :edit, :update, :destroy]
+	before_action :find_item, only: [:show, :edit, :update, :destroy]
 
 	def index
 		if user_signed_in?
-		@items = Item.where(:user_id => :current_user_id).order("created_at DESC")
+			@items = Item.where(:user_id => current_user.id).order("created_at DESC")
 		end
 	end
 
-
 	def show
-		
 	end
 
 	def new
@@ -17,21 +15,20 @@ class ItemsController < ApplicationController
 	end
 
 	def create
-	    @item = current_user.items.build(item_params)
-	    if @item.save
-		  redirect_to root_path
+		@item = current_user.items.build(item_params)
+		if @item.save
+			redirect_to root_path
 		else
-		  render 'new'
+			render 'new'
 		end
 	end
-
 
 	def edit
 	end
 
 	def update
 		if @item.update(item_params)
-			redirect_to @item 
+			redirect_to item_path(@item)
 		else
 			render 'edit'
 		end
@@ -42,21 +39,19 @@ class ItemsController < ApplicationController
 		redirect_to root_path
 	end
 
-
 	def complete
-		@item =Item.find(params[:id])
+		@item = Item.find(params[:id])
 		@item.update_attribute(:completed_at, Time.now)
 		redirect_to root_path
 	end
 
 	private
 
-
 	def item_params
-	 	params.require(:item).permit(:title, :description)
+		params.require(:item).permit(:title, :description)
 	end
 
 	def find_item
-		@item= Item.find(params[:id])
+		@item = Item.find(params[:id])
 	end
 end
