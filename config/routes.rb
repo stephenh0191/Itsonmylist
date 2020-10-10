335d94392754205f2ac06078d8dbeadfb45127c3
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-10 23:04:56 +0100

    Updated signin/login button

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	post "versions/:id/revert" => "versions#revert", :as => "revert_version"
 	devise_for :users
 	resources :items do
 		member do
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
 	end
 	root 'items#index'
 		get '/about' => 'page#about'
-		post "versions/:id/revert" => "versions#revert", :as => "revert_version"
+		
 end
