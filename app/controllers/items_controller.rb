ce399a8b0188808a4b6302fea6784c23366fe0a3
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-11 11:10:49 +0100

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
 	def update
 		if @item.update(item_params)
-			redirect_to item_path(@item), :notice =>  "Congrats! Task was
+			redirect_to item_path(@item), :notice =>  "Congrats! Task was updated!"
 		else
 			render 'edit'
 		end
