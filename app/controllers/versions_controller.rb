d550dd0a6d19fdab1f2a871c0010501cee131da2
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-03 19:42:48 +0000

    add edit user page to welcome dropdown

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,15 +1,17 @@
-# class VersionsController < ApplicationController
-# 		PaperTrail::Version.where('created_at < ?', 1.day.ago).delete_all
-# 		def revert
-# 			@version = Version.find(params[:id])
-# 			@version.reify
-# 			@version.reify.save!
-# 			else
-# 			@version.item.destroy
-# 			end
-# 			link_name = params[:redo] == "true" ? "undo" : "redo"
-# 			link = view_context.link_to(link_name, revert_version_path(, @Version.next, :redo => !params[:redo]), :method => :post)
-# 			redirect_to :back, :notice => "Undid #{@Version.event}. #{link}"
-# 		end
+class VersionsController < ApplicationController
+		PaperTrail::Version.where('created_at < ?', 1.day.ago).delete_all
+		def revert
+			@item = Item.find(params[:id])
+			if @item.blank?
+			format.html {redirect_to(rooth_path, :notice => 'Record not found') }
+			elsif @item.reify
+			@item.reify.save!
+			else
+			@item.item.destroy
+			end
+			link_name = params[:redo] == "true" ? "undo" : "redo"
+			link = view_context.link_to(link_name, revert_version_path(@item.next, :redo => !params[:redo]), :method => :post)
+			redirect_to :back, :notice => "Undid #{@item.event}. #{link}"
+		end
 	
-# end
+end
